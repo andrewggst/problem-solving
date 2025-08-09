d6ed2823438bf9558c43aacd366958b0be9367b1
Andrew Teles <andrewggst@gmail.com>
2025-08-08 23:15:57 -0300

    Update gerar_readme.py

--- .github/scripts/gerar_readme.py
+++ .github/scripts/gerar_readme.py
@@ -6,46 +6,47 @@ code_extensions = ['.java', '.py', '.cpp', '.c', '.js']
 base_folder = 'beecrowd'
 readme_path = os.path.join(base_folder, 'README.md')
 
-# Pega todas as pastas numeradas em beecrowd/
-exercise_folders = [f for f in os.listdir(base_folder) if os.path.isdir(os.path.join(base_folder, f)) and f.isdigit()]
+# Pega todas as pastas numeradas dentro de beecrowd/
+exercise_folders = [
+    f for f in os.listdir(base_folder)
+    if os.path.isdir(os.path.join(base_folder, f)) and f.isdigit()
+]
 exercise_folders = sorted(exercise_folders, key=lambda x: int(x))
 
-# Começa o conteúdo do README
+# Início do README
 readme_lines = [
-    '# Exercícios Beecrowd\n',
+    '# Exercícios Beecrowd\n\n',
     '| Exercício | Link do Problema | Soluções |\n',
     '|-----------|------------------|----------|\n'
 ]
 
 for folder in exercise_folders:
     folder_path = os.path.join(base_folder, folder)
-    # Filtra arquivos de código
-    code_files = [file for file in os.listdir(folder_path) if os.path.splitext(file)[1] in code_extensions]
+    code_files = [
+        file for file in os.listdir(folder_path)
+        if os.path.splitext(file)[1] in code_extensions
+    ]
 
     if not code_files:
-        # Pula pastas sem código
+        # Se não há código, ainda podemos mostrar a linha com aviso
+        readme_lines.append(
+            f'| {folder} | [Problema](https://www.beecrowd.com.br/judge/pt/problems/view/{folder}) | _Sem solução_ |\n'
+        )
         continue
 
-    # Link para o problema no beecrowd
-    problem_link = f'https://www.beecrowd.com.br/judge/pt/problems/view/{folder}'
-
-    # Links para as soluções (apenas nome do arquivo, com link para o arquivo no repo)
     solution_links = []
     for code_file in code_files:
-        # Link relativo para o arquivo
-        file_link = f'./{folder}/{code_file}'
-        # Exibe só a extensão maiúscula como label
         ext = os.path.splitext(code_file)[1].replace('.', '').upper()
+        file_link = f'{folder}/{code_file}'  # relativo à pasta beecrowd
         solution_links.append(f'[{ext}]({file_link})')
 
-    # Junta os links das soluções separados por espaço
     solutions_str = ' '.join(solution_links)
+    readme_lines.append(
+        f'| {folder} | [Problema](https://www.beecrowd.com.br/judge/pt/problems/view/{folder}) | {solutions_str} |\n'
+    )
 
-    # Monta a linha da tabela
-    readme_lines.append(f'| {folder} | [Problema]({problem_link}) | {solutions_str} |\n')
-
-# Escreve no README.md
+# Escreve o README
 with open(readme_path, 'w', encoding='utf-8') as f:
     f.writelines(readme_lines)
 
-print(f'Readme gerado com {len(readme_lines)-3} exercícios resolvidos.')
+print(f'✅ README gerado com {len(readme_lines)-3} exercícios listados.')
